d127531be70ece9cbea7bd620cb39e6041b4858f
Vactor0911 <vactor0911@gmail.com>
2024-09-13 21:32:53 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const Style = styled.div``;
+
+function App() {
+  return (
+    <Style>
+      <h1>Hello World!</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit quos
+        quas consectetur quia? Aliquid recusandae in tenetur voluptas ut
+        expedita nulla, ipsa cupiditate sint corporis, ipsam id? Commodi, beatae
+        ut.
+      </p>
+    </Style>
+  );
+}
+
+export default App;
