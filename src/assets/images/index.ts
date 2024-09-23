8f974593af97cdf4a6565032b5f3994cb79551ac
Vactor0911 <vactor0911@gmail.com>
2024-09-23 15:00:55 +0900

    Added game backgrounds

---
+++ src/assets/images/index.ts
@@ -0,0 +1,7 @@
+import Level1 from "../../assets/images/level1.png";
+import Level2 from "../../assets/images/level2.png";
+import Level3 from "../../assets/images/level3.png";
+import Level4 from "../../assets/images/level4.png";
+import Level5 from "../../assets/images/level5.png";
+import Level6 from "../../assets/images/level6.png";
+export { Level1, Level2, Level3, Level4, Level5, Level6 };
