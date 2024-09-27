f5d4162bdc5aa605bee66c97d1da99a7566fd8cb
Vactor0911 <vactor0911@gmail.com>
2024-09-27 09:08:16 +0900

    Added game bgm & fadeIn function

--- src/components/pages/Game.tsx
+++ src/components/pages/Game.tsx
@@ -1,11 +1,37 @@
 import styled from "styled-components";
 import { useAtomValue } from "jotai";
-import { gameDataAtom } from "../../state";
+import { audioBgmAtom, gameDataAtom } from "../../state";
+import { useEffect, useState } from "react";
+import { Music2 } from "../../assets/audio";
+import { audioVolume } from "../../utils";
 
 const Style = styled.div``;
 
 const Game = () => {
   const level = useAtomValue(gameDataAtom).level;
+
+  const audioBgm = useAtomValue(audioBgmAtom);
+
+  const [audioBgmVolume, setAudioBgmVolume] = useState(0.05);
+  useEffect(() => {
+    audioBgm.src = Music2;
+    audioBgm.currentTime = 0;
+    audioBgm.volume = audioBgmVolume;
+  }, [audioBgm]);
+
+  useEffect(() => {
+    const audioFadeIn = setInterval(() => {
+      setAudioBgmVolume((v) => v + 0.01);
+      audioBgm.volume = audioBgmVolume;
+    }, 100);
+
+    if (audioBgmVolume > audioVolume) {
+      clearInterval(audioFadeIn);
+    }
+
+    // 클리너
+    return () => clearInterval(audioFadeIn);
+  }, [audioBgm, audioBgmVolume]);
 
   return (
     <Style>
