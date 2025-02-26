266e922fc75393906ddda1efc736be678b45ce4b
Vactor0911 <vactor0911@gmail.com>
2025-02-26 23:21:34 +0900

    Implemented Main page

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,10 +1,31 @@
-import { Box, Typography } from "@mui/material";
+import { Box, keyframes, Stack } from "@mui/material";
+import Button from "../components/Button";
+
+const LogoAnimation = keyframes`
+  0% {
+    transform: rotate(-4deg);
+  }
+  50% {
+    transform: rotate(4deg);
+  }
+  100% {
+    transform: rotate(-4deg);
+  }
+`;
 
 const Main = () => {
   return (
-    <Box>
-      <Typography variant="h1">Hello, World!</Typography>
-    </Box>
+    <Stack alignItems="center" justifyContent="center" height="100vh" gap={3}>
+      <Box
+        component="img"
+        alt="Logo"
+        src="./Logo.png"
+        width="600px"
+        sx={{ animation: `${LogoAnimation} 4s ease-in-out infinite` }}
+      />
+      <Button text="Start Game" />
+      <Button text="How to Play" />
+    </Stack>
   );
 };
 
