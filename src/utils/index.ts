cc0e5f524995b98ebf05771cca4991425e24e958
Vactor0911 <vactor0911@gmail.com>
2025-03-05 22:42:22 +0900

    Adjusted default audio volumes

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -64,14 +64,14 @@ export const theme = createTheme({
 });
 
 // 오디오 재생
-export const playMusic = (src: string, volume = 1) => {
+export const playMusic = (src: string, volume = 0.3) => {
   musicAudio.src = src;
   musicAudio.volume = volume;
   musicAudio.play();
 };
 
 // 효과음 재생
-export const playEffect = (src: string, volume = 1) => {
+export const playEffect = (src: string, volume = 0.3) => {
   for (const audio of effectAudios) {
     if (audio.paused) {
       audio.src = src;
