80b4896237e7b6040d43f25dfcf0ba7d7679ebf9
Vactor0911 <vactor0911@gmail.com>
2025-03-08 01:45:47 +0900

    Added level preview images

---
+++ src/assets/images/levels/index.ts
@@ -0,0 +1,15 @@
+import Level1Preview from "./level1_preview.svg";
+import Level2Preview from "./level2_preview.svg";
+import Level3Preview from "./level3_preview.svg";
+import Level4Preview from "./level4_preview.svg";
+import Level5Preview from "./level5_preview.svg";
+import Level6Preview from "./level6_preview.svg";
+
+export {
+  Level1Preview,
+  Level2Preview,
+  Level3Preview,
+  Level4Preview,
+  Level5Preview,
+  Level6Preview,
+};
