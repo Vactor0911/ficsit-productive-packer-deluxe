77b63e78fe795d4de9531d138a9177806010cc99
Vactor0911 <vactor0911@gmail.com>
2025-03-19 14:04:08 +0900

    Added breakpoints for tablet & mobile browsers

--- src/components/Scoreboard.tsx
+++ src/components/Scoreboard.tsx
@@ -1,4 +1,4 @@
-import { Stack, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import Panel from "./Panel";
 import ScoreTimer from "./ScoreTimer";
 import Coin from "../assets/images/coin.svg";
@@ -8,14 +8,36 @@ const Scoreboard = () => {
     <Panel padding={1}>
       <Stack direction="row" gap={2}>
         {/* 제한시간 */}
-        <ScoreTimer time={20} maxTime={100} isTimeAlert />
+        <ScoreTimer />
 
         {/* 점수 */}
-        <Stack gap={3}>
+        <Stack
+          gap={{
+            md: 3,
+            xs: 2,
+          }}
+        >
           <Typography variant="h4">최종 점수 :</Typography>
           <Stack direction="row" gap={1} alignItems="center">
-            <img src={Coin} alt="coin" width="80px" height="80px" />
-            <Typography variant="h1" fontSize="5em" lineHeight="1em">
+            <Box
+              component="img"
+              src={Coin}
+              alt="coin"
+              width={{
+                md: "80px",
+                sm: "70px",
+                xs: "45px",
+              }}
+            />
+            <Typography
+              variant="h1"
+              fontSize={{
+                md: "5em",
+                sm: "4em",
+                xs: "2.5em",
+              }}
+              lineHeight="1em"
+            >
               100
             </Typography>
           </Stack>
---
+++ src/states/index.ts
@@ -0,0 +1,5 @@
+import { atom } from "jotai";
+
+export const timeLeftAtom = atom(95);
+
+export const maxTimeAtom = atom(95);
