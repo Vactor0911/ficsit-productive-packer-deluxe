b11dfbb37607972c4040564c40ab59a6a3b723b4
Vactor0911 <vactor0911@gmail.com>
2025-03-20 15:15:22 +0900

    Implemented block container style

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -11,9 +11,18 @@ const Game = () => {
 
   return (
     <Stack gap={1} height="100vh" position="relative">
-      <Stack width="90%" maxWidth="650px" marginTop={2} alignSelf="center">
+      {/* 점수판 */}
+      <Stack
+        width="90%"
+        maxWidth="650px"
+        marginTop={2}
+        alignSelf="center"
+        position="relative"
+        zIndex={5}
+      >
         <Scoreboard />
       </Stack>
+
       <Box
         height={{
           md: "70vh",
@@ -21,9 +30,14 @@ const Game = () => {
         }}
         position="relative"
       >
-        <Conveyor height="100%" minHeight="300px" />
+        {/* 컨베이어 벨트트 */}
+        <Conveyor height="100%" />
+
+        {/* PC, 태블릿용 블록 컨테이너 */}
         <BlockContainer />
       </Box>
+
+      {/* 모바일용 블록 컨테이너 */}
       <MobileBlockContainer />
     </Stack>
   );
