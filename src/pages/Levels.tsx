61c3d286c5362468db9129ebc7adc57f6b2acc49
Vactor0911 <vactor0911@gmail.com>
2025-06-30 23:49:15 +0900

    Implemented Levels page

--- src/pages/Levels.tsx
+++ src/pages/Levels.tsx
@@ -1,5 +1,173 @@
+import {
+  Box,
+  ButtonBase,
+  Container,
+  keyframes,
+  Stack,
+  Typography,
+} from "@mui/material";
+import {
+  Level1,
+  Level2,
+  Level3,
+  Level4,
+  Level5,
+  Level6,
+} from "../assets/images/levels";
+import Button from "../components/Button";
+import StarTwoToneIcon from "@mui/icons-material/StarTwoTone";
+import { useCallback, useMemo } from "react";
+import { useNavigate } from "react-router-dom";
+
+// 게임 레벨 데이터
+const levels = [
+  {
+    level: 1,
+    stars: 0,
+    maxScore: "",
+    image: Level1,
+  },
+  {
+    level: 2,
+    stars: 0,
+    maxScore: "",
+    image: Level2,
+  },
+  {
+    level: 3,
+    stars: 0,
+    maxScore: "",
+    image: Level3,
+  },
+  {
+    level: 4,
+    stars: 0,
+    maxScore: "",
+    image: Level4,
+  },
+  {
+    level: 5,
+    stars: 0,
+    maxScore: "",
+    image: Level5,
+  },
+  {
+    level: 6,
+    stars: 0,
+    maxScore: "",
+    image: Level6,
+  },
+];
+
 const Levels = () => {
-  return <></>;
+  const navigate = useNavigate();
+
+  // 호버 애니메이션
+  const hoverAnimation = useMemo(
+    () =>
+      keyframes({
+        "0%": {
+          transform: "translateX(-100%)",
+        },
+        "100%": {
+          transform: "translateX(100%)",
+        },
+      }),
+    []
+  );
+
+  // 메인 화면으로 버튼 클릭
+  const handleMainButtonClick = useCallback(() => {
+    navigate("/");
+  }, [navigate]);
+
+  return (
+    <Container maxWidth="md">
+      <Stack gap={2} paddingY={3}>
+        {levels.map((level) => (
+          <ButtonBase
+            key={`level-button-${level.level}`}
+            disableRipple
+            sx={{
+              width: "100%",
+              boxShadow: `0 8px 0 rgba(0, 0, 0, 0.15)`,
+            }}
+          >
+            <Stack
+              width="100%"
+              direction="row"
+              alignItems="center"
+              padding={2}
+              paddingY={1}
+              gap={2}
+              sx={{
+                backgroundColor: "white",
+                border: "2px solid black",
+                position: "relative",
+                overflow: "hidden",
+                "&:hover": {
+                  backgroundColor: "#b6e5d6",
+                  "&:before": {
+                    content: '""',
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    width: "200%",
+                    height: "100%",
+                    zIndex: 2,
+                    transform: "translateX(-100%)",
+                    background: `linear-gradient(
+                      150deg,
+                      transparent 40%,
+                      rgba(255, 255, 255, 0.25) 40%,
+                      rgba(255, 255, 255, 0.25) 60%,
+                      transparent 60%
+                    )`,
+                    animation: `${hoverAnimation} 0.6s linear`,
+                  },
+                },
+              }}
+            >
+              {/* 레벨 이미지 */}
+              <Box
+                component="img"
+                alt={`Level ${level.level}`}
+                src={level.image}
+                height="100px"
+              />
+
+              {/* 레벨 정보 */}
+              <Stack flex={1} textAlign="left">
+                {/* 레벨 */}
+                <Typography variant="h5">레벨 {level.level}</Typography>
+
+                {/* 별 */}
+                <Stack direction="row" alignItems="center">
+                  <StarTwoToneIcon fontSize="large" />
+                  <StarTwoToneIcon fontSize="large" />
+                  <StarTwoToneIcon fontSize="large" />
+                </Stack>
+
+                {/* 최고 점수 */}
+                <Typography variant="subtitle1">최고 점수:</Typography>
+
+                <Typography variant="h6" color="primary">
+                  홍길동: 1,234
+                </Typography>
+              </Stack>
+            </Stack>
+          </ButtonBase>
+        ))}
+
+        {/* 메인 화면으로 버튼 */}
+        <Box marginX="auto" marginY={5}>
+          <Button onClick={handleMainButtonClick}>
+            <Typography variant="h4">메인 화면으로</Typography>
+          </Button>
+        </Box>
+      </Stack>
+    </Container>
+  );
 };
 
 export default Levels;
--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -32,6 +32,24 @@ export const theme = responsiveFontSizes(
     },
     typography: {
       fontFamily: "Noto Sans KR, Noto Sans, sans-serif",
+      h1: {
+        fontWeight: "bold",
+      },
+      h2: {
+        fontWeight: "bold",
+      },
+      h3: {
+        fontWeight: "bold",
+      },
+      h4: {
+        fontWeight: "bold",
+      },
+      h5: {
+        fontWeight: "bold",
+      },
+      h6: {
+        fontWeight: "bold",
+      },
     },
   })
 );
