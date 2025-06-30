e58f8d2240cc7df107e21395a254656e272bc441
Vactor0911 <vactor0911@gmail.com>
2025-06-30 23:48:27 +0900

    Added level image assets

---
+++ src/assets/images/levels/index.ts
@@ -0,0 +1,15 @@
+import Level1 from "./level1.svg";
+import Level2 from "./level2.svg";
+import Level3 from "./level3.svg";
+import Level4 from "./level4.svg";
+import Level5 from "./level5.svg";
+import Level6 from "./level6.svg";
+
+export {
+  Level1,
+  Level2,
+  Level3,
+  Level4,
+  Level5,
+  Level6,
+};
