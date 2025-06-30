f118ffc4548861e006dffdb6376fafe4958dde48
Vactor0911 <vactor0911@gmail.com>
2025-06-30 18:37:05 +0900

    Emotion setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      jsxImportSource: "@emotion/react",
+    }),
+  ],
+  base: "/ficsit-productive-packer-deluxe",
+  server: {
+    port: 8080,
+    allowedHosts: ["0.tcp.jp.ngrok.io"],
+  },
+});
