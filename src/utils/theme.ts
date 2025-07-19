b32acad0146e533fff7d9dbbdcd3949ce351eed1
Vactor0911 <vactor0911@gmail.com>
2025-07-20 07:52:33 +0900

    Removed black theme

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,30 +1,10 @@
 import { createTheme, responsiveFontSizes } from "@mui/material";
 
-// MUI Palette 확장
-declare module "@mui/material/styles" {
-  interface Palette {
-    black: Palette["primary"];
-  }
-  interface PaletteOptions {
-    black?: PaletteOptions["primary"];
-  }
-}
-
-// MUI Typography 색상 확장
-declare module "@mui/material/Typography" {
-  interface TypographyPropsColorOverrides {
-    black: true;
-  }
-}
-
 export const theme = responsiveFontSizes(
   createTheme({
     palette: {
       primary: {
         main: "#e59344",
-      },
-      black: {
-        main: "#404040",
       },
       background: {
         default: "#29a995",
