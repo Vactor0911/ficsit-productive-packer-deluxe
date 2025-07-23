2c6b288f8cb098a5316e470befbd9c4408922df7
Vactor0911 <vactor0911@gmail.com>
2025-07-23 13:49:37 +0900

    Implemented Timer component

---
+++ src/components/Timer.tsx
@@ -0,0 +1,51 @@
+import { Stack, Typography, useTheme } from "@mui/material";
+
+const Timer = () => {
+  const theme = useTheme();
+
+  return (
+    <Stack
+      height="100%"
+      justifyContent="center"
+      alignItems="center"
+      borderRadius="50%"
+      border="2px solid black"
+      sx={{
+        aspectRatio: "1 / 1",
+        "& .MuiStack-root": {
+          borderRadius: "50%",
+        },
+      }}
+    >
+      <Stack
+        width="95%"
+        height="95%"
+        justifyContent="center"
+        alignItems="center"
+        bgcolor={theme.palette.primary.main}
+      >
+        <Stack
+          width="75%"
+          height="75%"
+          justifyContent="center"
+          alignItems="center"
+          bgcolor="white"
+        >
+          <Stack
+            width="90%"
+            height="90%"
+            alignItems="center"
+            paddingTop={1}
+            bgcolor="#666666"
+            color="white"
+          >
+            <Typography variant="subtitle2">시간:</Typography>
+            <Typography variant="h5">12.3초</Typography>
+          </Stack>
+        </Stack>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default Timer;
