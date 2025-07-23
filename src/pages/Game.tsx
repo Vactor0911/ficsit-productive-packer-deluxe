3ebfdbef6ea632d221a55e1578a0f3973f353faa
Vactor0911 <vactor0911@gmail.com>
2025-07-23 13:50:37 +0900

    Implemented total score board

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,5 +1,57 @@
+import { Box, Container, Stack, Typography } from "@mui/material";
+import Panel from "../components/Panel";
+import Score from "../components/Score";
+import { useState } from "react";
+import Timer from "../components/Timer";
+
 const Game = () => {
-  return <></>;
+  const [score, setScore] = useState(1234);
+
+  // const handleScoreAdd = useCallback(() => {
+  //   setScore((prevScore) => prevScore + Math.floor(Math.random() * 1000));
+  // }, []);
+
+  return (
+    <Stack height="100vh">
+      {/* 점수 판 */}
+      <Container maxWidth="md">
+        <Panel height="17vh" marginTop="2vh" backgroundColor="#fffff7">
+          <Stack
+            height="100%"
+            direction="row"
+            padding="1vh"
+            paddingY="1.3vh"
+            gap={1}
+          >
+            {/* 타이머 */}
+            <Timer />
+
+            <Stack gap={1} flex={1}>
+              {/* 헤더 */}
+              <Typography variant="subtitle1">최종 점수 :</Typography>
+
+              {/* 최종 점수 */}
+              <Box
+                sx={{
+                  "& .MuiTypography-root": {
+                    fontSize: "4.5rem",
+                  },
+                }}
+              >
+                <Score
+                  variant="total"
+                  score={score}
+                  typographyProps={{
+                    variant: "h2",
+                  }}
+                />
+              </Box>
+            </Stack>
+          </Stack>
+        </Panel>
+      </Container>
+    </Stack>
+  );
 };
 
 export default Game;
