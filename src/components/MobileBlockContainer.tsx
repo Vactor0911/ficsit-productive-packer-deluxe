ba3f954dbfe347ba30bbdf517d3fe2e6cc88c7a3
Vactor0911 <vactor0911@gmail.com>
2025-08-11 08:14:03 +0900

    Added bolt decorations to block container

--- src/components/MobileBlockContainer.tsx
+++ src/components/MobileBlockContainer.tsx
@@ -1,18 +1,23 @@
 import { Box, Stack } from "@mui/material";
+import Bolt from "./Bolt";
 
 const MobileBlockContainer = () => {
   return (
     <Stack width="100%" height="100%" position="relative">
       {/* 상부 장식 */}
       <Box
-        height="16px"
+        height="12px"
         bgcolor="#4d4d4d"
         borderTop="2px solid black"
         borderBottom="2px solid black"
       />
 
       {/* 블록 컨테이너 */}
-      <Box flex={1} bgcolor="#666666"></Box>
+      <Box flex={1} bgcolor="#666666" position="relative">
+        {/* 볼트 장식 */}
+        <Bolt top={8} left={8} />
+        <Bolt top={8} right={8} />
+      </Box>
     </Stack>
   );
 };
