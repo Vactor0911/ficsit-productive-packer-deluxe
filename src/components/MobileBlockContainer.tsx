66165f18d6bd69bccbf47cf470b946b534c8ec09
Vactor0911 <vactor0911@gmail.com>
2025-08-12 15:17:55 +0900

    Implemented mobile block container

--- src/components/MobileBlockContainer.tsx
+++ src/components/MobileBlockContainer.tsx
@@ -1,7 +1,12 @@
 import { Box, Stack } from "@mui/material";
 import Bolt from "./Bolt";
+import { useAtom } from "jotai";
+import { blockIdQueueAtom } from "../states";
+import Block from "./Block";
 
 const MobileBlockContainer = () => {
+  const [blockIdQueue] = useAtom(blockIdQueueAtom);
+
   return (
     <Stack width="100%" height="100%" position="relative">
       {/* 상부 장식 */}
@@ -13,10 +18,29 @@ const MobileBlockContainer = () => {
       />
 
       {/* 블록 컨테이너 */}
-      <Box flex={1} bgcolor="#666666" position="relative">
+      <Box height="calc(100% - 12px)" bgcolor="#666666" position="relative">
         {/* 볼트 장식 */}
         <Bolt top={8} left={8} />
         <Bolt top={8} right={8} />
+
+        <Stack
+          direction="row"
+          width="100%"
+          height="100%"
+          flexWrap="wrap"
+          padding={1}
+          paddingTop={2}
+        >
+          {blockIdQueue.map((blockId, index) => (
+            <Block
+              key={`block-${index}`}
+              id={`block-${index}`}
+              width="25%"
+              height="50%"
+              blockId={blockId}
+            />
+          ))}
+        </Stack>
       </Box>
     </Stack>
   );
--- src/states/index.ts
+++ src/states/index.ts
@@ -2,3 +2,6 @@ import { atom } from "jotai";
 
 // vh 단위 보정
 export const vhAtom = atom(window.innerHeight * 0.01);
+
+// 블록 큐
+export const blockIdQueueAtom = atom<number[]>([0, 5, 8, 12, 11, 20, 23, 18]);
