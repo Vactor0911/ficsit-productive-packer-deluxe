c0c1ea081dd8dbd059caeb373e99e59c4743980b
Vactor0911 <vactor0911@gmail.com>
2025-08-15 10:17:06 +0900

    Added package cover

--- src/components/Package.tsx
+++ src/components/Package.tsx
@@ -1,5 +1,6 @@
 import { Box, Stack, Typography } from "@mui/material";
 import Board from "./Board";
+import FicsitLogo from "../assets/images/ficsit.svg";
 
 const Package = () => {
   return (
@@ -9,6 +10,7 @@ const Package = () => {
       bgcolor="#e5b065"
       border="2px solid black"
       boxShadow="0 24px 0 rgba(0, 0, 0, 0.2)"
+      position="relative"
     >
       <Stack flex={1} margin={0.5} border="2px solid black">
         {/* 패키지 내벽 */}
@@ -25,9 +27,10 @@ const Package = () => {
           alignItems="center"
           position="relative"
         >
-          {/* 패키지 하부 */}
+          {/* 패키지 하부 장식 */}
           <Box width="100%" height="2px" bgcolor="#8d5b1d" />
 
+          {/* 보드 */}
           <Stack
             width="100%"
             height="100%"
@@ -68,6 +71,52 @@ const Package = () => {
             Ficsit
           </Typography>
         </Box>
+      </Stack>
+
+      {/* 덮개 */}
+      <Stack
+        width="calc(100% + 4px)"
+        height="calc(100% - 12px)"
+        position="absolute"
+        top="-2px"
+        left="-2px"
+        zIndex={1000}
+      >
+        <Stack
+          flex={1}
+          justifyContent="center"
+          bgcolor="#e5b065"
+          border="2px solid black"
+          position="relative"
+        >
+          {/* 덮개 장식 */}
+          <Box width="100%" height="2px" bgcolor="#8d5b1d" />
+
+          {/* 덮개 문구 */}
+          <Stack
+            alignItems="flex-end"
+            color="#8d5b1d"
+            position="absolute"
+            bottom={10}
+            right={10}
+          >
+            <Box component="img" src={FicsitLogo} width="3.5rem" />
+            <Typography variant="subtitle1" lineHeight="1rem">
+              DELIVERY
+            </Typography>
+            <Typography variant="subtitle1" lineHeight="1rem">
+              SERVICES
+            </Typography>
+          </Stack>
+        </Stack>
+
+        {/* 덮개 외벽 */}
+        <Box
+          height="8px"
+          bgcolor={"#8d5b1d"}
+          border="2px solid black"
+          borderTop="none"
+        />
       </Stack>
     </Stack>
   );
