d7300c7c8726b58c5e8eae7ae1604650b25d848a
Vactor0911 <vactor0911@gmail.com>
2025-10-01 02:19:55 +0900

    Refactored StyledOverlayScrollbarsComponent

--- src/components/StyledOverlayScrollbarsComponent.tsx
+++ src/components/StyledOverlayScrollbarsComponent.tsx
@@ -1,14 +1,40 @@
-import styled from "@emotion/styled";
-import { OverlayScrollbarsComponent } from "overlayscrollbars-react";
+import {
+  OverlayScrollbarsComponent,
+  type OverlayScrollbarsComponentProps,
+} from "overlayscrollbars-react";
 import "overlayscrollbars/overlayscrollbars.css";
+import type { ReactNode } from "react";
 
-const StyledOverlayScrollbarsComponent = styled(OverlayScrollbarsComponent)`
-  height: 100%;
-  .os-scrollbar {
-    --os-size: 12px;
-    --os-handle-bg: white;
-    --os-handle-border-radius: 2px;
-    --os-handle-border: 1px solid #aaa;
-`;
+interface StyledOverlayScrollbarsComponent
+  extends OverlayScrollbarsComponentProps {
+  children: ReactNode;
+  css?: React.CSSProperties;
+}
+
+const StyledOverlayScrollbarsComponent = (
+  props: StyledOverlayScrollbarsComponent
+) => {
+  const { children, css, ...others } = props;
+
+  return (
+    <OverlayScrollbarsComponent
+      css={{
+        height: "100%",
+        "& .os-scrollbar": {
+          "--os-size": "12px",
+          "--os-handle-bg": "white",
+          "--os-handle-border-radius": "2px",
+          "--os-handle-border": "1px solid #aaa",
+          position: "absolute",
+          zIndex: 10000,
+        },
+        ...css,
+      }}
+      {...others}
+    >
+      {children}
+    </OverlayScrollbarsComponent>
+  );
+};
 
 export default StyledOverlayScrollbarsComponent;
